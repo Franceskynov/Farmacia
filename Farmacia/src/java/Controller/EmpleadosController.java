4780f6ad704bddacf85dfecb2c2b5cc0a7955729
Miguel <Miguel@192.168.0.20>
2017-11-20 07:38:05 -0600

    se agregaron los metodos de control en EmpleadosController.java: limpiarEmpleado(), agregarEmpleado(), eliminarEmpleado(), modificarEmpleado().

--- Farmacia/src/java/Controller/EmpleadosController.java
+++ Farmacia/src/java/Controller/EmpleadosController.java
@@ -8,16 +8,14 @@ package Controller;
 import Model.Dao.EmpleadosDao;
 import Model.Entities.Empleados;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 //import javax.inject.Named;
 //import javax.faces.view.ViewScoped;
 
-/**
- *
- * @author Josué Mercally
- */
+
 //@Named(value = "Empleados")
 @ManagedBean
 @ViewScoped
@@ -27,6 +25,7 @@ public class EmpleadosController implements Serializable {
     }
     
     private List<Empleados> listaEmpleados;
+    private Empleados empleado;
     
     public void setListaEmpleados(List<Empleados> listaEmpleados) {
         this.listaEmpleados = listaEmpleados;
@@ -37,4 +36,35 @@ public class EmpleadosController implements Serializable {
         this.listaEmpleados = Dao.ListaEmpleados();
         return this.listaEmpleados;
     } 
+
+    public Empleados getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleados empleado) {
+        this.empleado = empleado;
+    }
+    
+    public void limpiarEmpleado() {
+        empleado = new Empleados();
+        empleado.setFechaIngreso(new Date());
+    }
+
+    public void agregarEmpleado() {
+        EmpleadosDao ed = new EmpleadosDao();
+        ed.agregar(empleado);
+    }
+
+    public void modificarUsuario() {
+        EmpleadosDao ud = new EmpleadosDao();
+        ud.modificar(empleado);
+        limpiarEmpleado();
+    }
+
+    public void eliminarUsuario() {
+        EmpleadosDao ud = new EmpleadosDao();
+        ud.eliminar(empleado);
+        limpiarEmpleado();
+    }
+    
 }
