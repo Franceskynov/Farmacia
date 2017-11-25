eb49a811d4ff43db93d7c008ccfce1cc682728eb
Miguel <Miguel@192.168.0.20>
2017-11-25 08:51:35 -0600

    se agrego los metodos de mantenimiento listarClientes(), agregar(), modificar(), Eliminar().

--- Farmacia/src/java/Model/Dao/ClientesDao.java
+++ Farmacia/src/java/Model/Dao/ClientesDao.java
@@ -5,10 +5,81 @@
  */
 package Model.Dao;
 
+import Model.Entities.Clientes;
+import Model.Util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author Miguel
  */
 public class ClientesDao {
     
+    public List<Clientes> listarClientes() {
+        List<Clientes> lista = null;
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        Transaction t = sesion.beginTransaction();
+        String hql = "FROM Clientes";
+        try {
+            lista = sesion.createQuery(hql).list();
+            t.commit();
+            sesion.close();
+        } catch (Exception e) {
+            t.rollback();
+        }
+        return lista;
+    }
+
+    public void agregar(Clientes cliente) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.save(cliente);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void modificar(Clientes cliente) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.update(cliente);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+
+    public void eliminar(Clientes cliente) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.delete(cliente);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
 }
