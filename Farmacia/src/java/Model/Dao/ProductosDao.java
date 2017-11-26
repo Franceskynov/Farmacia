65d953f1a6ec422b7800a1c21c2ddfcdce5550ab
Miguel <Miguel@192.168.0.20>
2017-11-26 15:20:32 -0600

    se agregaron los metodos de mantenimiento lista(), agregar(), modificar(), eliminar(), a los java beans ProductosDao y ProveedoresDao

--- Farmacia/src/java/Model/Dao/ProductosDao.java
+++ Farmacia/src/java/Model/Dao/ProductosDao.java
@@ -1,10 +1,82 @@
-
 package Model.Dao;
+
+import Model.Entities.Productos;
+import Model.Util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author Miguel
  */
 public class ProductosDao {
+    
+    public List<Productos> ListaProductos(){
+        List<Productos> lista = null;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tran = session.beginTransaction();
+        String hql = "FROM Productos";
+        try{
+            lista = session.createQuery(hql).list();
+            tran.commit();
+            session.close();
+        }catch(Exception ex){
+            tran.rollback();
+        }
+        
+        return lista;
+    }
+    
+    public void agregar(Productos producto) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.save(producto);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+    
+    public void modificar(Productos producto) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.update(producto);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
+    
+    public void eliminar(Productos producto) {
+        Session sesion = null;
+        try {
+            sesion = HibernateUtil.getSessionFactory().openSession();
+            sesion.beginTransaction();
+            sesion.delete(producto);
+            sesion.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            sesion.getTransaction().rollback();
+        } finally {
+            if (sesion != null) {
+                sesion.close();
+            }
+        }
+    }
     
 }
